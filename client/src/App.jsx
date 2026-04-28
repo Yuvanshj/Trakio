46b88fc7b5d657a98affcc77431777f0a1df772a
Shresth Juyal <shresthajuyal96@gmail.com>
2026-04-28 22:32:49 +0530

    Tailwind Css setup
    Tailwind Css setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 
 import './App.css'
-
-import React from 'react'
-
+import { BrowserRouter,Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/Login"
+import Signup from './pages/auth/Signup';
 function App() {
   return (
-    <div>App
-      <p>Good Morning Pineapple</p>
+    <div className="bg-black text-white min-h-screen overflow-x-hidden">
+   <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+      </Routes>
+    </BrowserRouter>
     </div>
   )
 }
