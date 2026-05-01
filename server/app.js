96dcb06c4c2ca9e9ee4b95a2f44bf4bea6e6e1d2
Yuvansh Juneja <vujj892@gmail.com>
2026-05-02 01:22:25 +0530

    initialized leaflet map.

--- server/app.js
+++ server/app.js
@@ -6,8 +6,12 @@ const http = require("http")
 
 const socketio = require("socket.io")
 const server = http.createServer(app)
-
 const io = socketio(server)
+
+app.set("view engine", "ejs")
+app.set(express.static(path.join(__dirname, "public")))
+ 
+
 
 app.get("/", (req, res)=>{
     res.send("hey")
