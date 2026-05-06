ba6cc48cb78e6285543d304c341813567a50eb63
Yashaswi Gusain <gusainyashaswi@gmail.com>
2026-05-06 23:57:16 +0530

    About Page Update

---
+++ client/src/pages/About/AboutHero.jsx
@@ -0,0 +1,14 @@
+export default function AboutHero() {
+  return (
+    <section className="hero relative flex flex-col items-center justify-center overflow-hidden bg-[#f5f5f5] py-32">
+      <h1 className="bg-text absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 whitespace-nowrap font-bebas text-[220px] leading-none tracking-[-10px] text-black/5 select-none z-0">THE TRAKIO</h1>
+      <div className="hero-content relative z-10 text-center">
+        <h2 className="main-heading font-bebas uppercase text-[#111] leading-[0.9] tracking-[-4px] text-[120px]">
+          CONNECT WITH <br />
+          OUR TEAM
+        </h2>
+      </div>
+
+    </section>
+  );
+}
