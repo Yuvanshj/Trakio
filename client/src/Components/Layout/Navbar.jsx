98778eace740a64f088c13a9fddd61037d866c47
Shresth Juyal <shresthajuyal96@gmail.com>
2026-05-07 13:24:48 +0530

    Few navbar updates
    Few navbar updates

--- client/src/Components/Layout/Navbar.jsx
+++ client/src/Components/Layout/Navbar.jsx
@@ -29,26 +29,36 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="w-full h-15 bg-white border-b border-gray-200 px-6 flex items-center justify-between">
-      
-      <Link to="/" className="flex items-center justify-center">
-        <img src={trakioLogo} alt="Trakio Logo" className="h-20 w-auto mt-4 ml-0" />
-      </Link>
+    <nav className="w-full h-17 bg-white border-b border-gray-200 px-6 flex items-center">
 
-      <div className="flex items-center gap-6">
-        <Link to="/" className="text-gray-700 hover:text-black">
-          Home
+      <div className="flex items-center mt-3">
+        <Link to="/" className="flex items-center">
+          <img src={trakioLogo} alt="Trakio Logo" className="h-14 md:h-16 w-auto" />
         </Link>
+<p className="text-black text-xl font-bold tracking-tight items-center justify-between mb-2">
+  Trakio
+</p>      </div>
 
-        <Link to="/track" className="text-gray-700 hover:text-black">
-          Track
-        </Link>
+      <div className="flex-3 flex items-center justify-center">
+        <div className="bg-gray-100 rounded-full px-6 py-1 flex items-center gap-4 shadow-sm">
+          <Link to="/" className="px-3 md:px-4 py-2 rounded-full text-gray-700 hover:text-black transition-colors">
+            Home
+          </Link>
 
-        <Link to="/about" className="text-gray-700 hover:text-black">
-          About
-        </Link>
+          <Link to="/track" className="px-3 md:px-4 py-2 rounded-full text-gray-700 hover:text-black transition-colors">
+            Track
+          </Link>
+
+          <Link to="/about" className="px-3 md:px-4 py-2 rounded-full text-gray-700 hover:text-black transition-colors">
+            About
+          </Link>
+          <Link to="/" className="px-3 md:px-4 py-2 rounded-full text-gray-700 hover:text-black transition-colors">
+            App
+          </Link>
+        </div>
       </div>
 
+      
       <div className="flex items-center gap-4">
         {user ? (
           <>
