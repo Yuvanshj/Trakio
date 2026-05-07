f86b3225ee424d2a387d6ae4fc876bcbace98691
Yuvansh Juneja <vujj892@gmail.com>
2026-05-08 00:18:59 +0530

    somethin somethin

--- client/src/pages/About/ConnectBanner.jsx
+++ client/src/pages/About/ConnectBanner.jsx
@@ -1,112 +1,40 @@
+import ScrollVelocity from '../../Components/ScrollVelocity/ScrollVelocity';
+
+
 export default function ConnectBanner() {
 
-  const items = [
-    "Instagram",
-    "LinkedIn",
-    "GitHub",
-    "Mail",
-    "The Trakio",
-    "Connect",
-    "Collaborate",
-    "Instagram",
-    "LinkedIn",
-    "GitHub",
-    "Mail",
-    "The Trakio",
-    "Connect",
-    "Collaborate",
-    "Instagram",
-    "LinkedIn",
-    "GitHub",
-    "Mail",
-    "The Trakio",
-    "Connect",
-    "Collaborate",
-  ];
+  const marqueeText = "Instagram • LinkedIn • GitHub • Mail • The Trakio • Connect • Collaborate •";
 
   return (
     <section
       className="
-        group
         relative
         overflow-hidden
-
         bg-black
-        
-
-        py-8
-
+        py-2
         transition-all
         duration-700
-
       "
     >
 
       <div className="absolute top-0 left-0 w-full h-[1px] bg-white/20" />
-
       <div className="absolute bottom-0 left-0 w-full h-[1px] bg-black" />
-      <div className="connect-marquee">
 
-        <div
-          className="
-            connect-marquee-content
-            transition-all
-            duration-700
-
-           
-          "
-        >
-
-          {[...items, ...items].map((item, index) => (
-
-            <div
-              key={index}
-              className="
-                flex
-                items-center
-                gap-6
-              "
-            >
-
-              <span
-                className="
-                  uppercase
-                  tracking-[4px]
-
-                  text-2xl
-                  md:text-base
-                  font-extrabold
-
-                  text-white
-
-                "
-              >
-                {item}
-              </span>
-
-
-              <span
-                className="
-                  text-white
-                  text-xl
-
-                  transition-all
-                  duration-500
-
-                  group-hover:text-white
-                  group-hover:scale-150
-                "
-              >
-                •
-              </span>
-
-            </div>
-
-          ))}
-
-        </div>
-
-      </div>
+      <ScrollVelocity
+        texts={[marqueeText]}
+        velocity={80}
+        numCopies={4}
+        className="
+          uppercase
+          tracking-tighter
+          text-xl
+          md:text-3xl
+          font-black
+          text-white
+        "
+        parallaxStyle={{ padding: '2px 0' }}
+        scrollerStyle={{ gap: '0px' }}
+      />
 
     </section>
   );
