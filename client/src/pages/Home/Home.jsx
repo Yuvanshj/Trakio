24f906b136154d1ce2197c0ba17e3bf26063919f
Yashaswi Gusain <gusainyashaswi@gmail.com>
2026-05-08 04:59:07 +0530

    Update Home.jsx

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import HeroSection from "./HeroSection";
 import Footer from "../../Components/Layout/Footer";
+import TrustSection from "./TrustSection";
 
 export default function Home() {
   return (
     <main className="bg-black">
       <HeroSection />
+      <TrustSection/>
       <Footer />
+
     </main>
   );
 }
