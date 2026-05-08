806450c79987f613af46076ea3659e50077a59be
Shresth Juyal <shresthajuyal96@gmail.com>
2026-05-08 23:23:55 +0530

    .
    .

--- client/src/pages/About/AboutHero.jsx
+++ client/src/pages/About/AboutHero.jsx
@@ -14,21 +14,21 @@ export default function AboutHero() {
     {
       id: 1,
       name: "Yashaswi Gusain",
-      role: "Talwinder God",
+      role: ".",
       image: Yash,
       about: "Passionate about crafting immersive frontend experiences and modern UI systems.",
     },
     {
       id: 2,
       name: "Shresth Juyal",
-      role: "Yashaswi's Son",
+      role: ".",
       image: Alien,
       about: "Focused on scalable backend architecture, APIs, and performance optimization.",
     },
     {
       id: 3,
       name: "Yuvansh Juneja",
-      role: "Yashaswi's Tatta",
+      role: ".",
       image: Yuvi,
       about:"Designs elegant user experiences with strong attention to detail and storytelling.",
     }
