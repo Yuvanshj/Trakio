41bcaf2f0e197d8a65915b78260c11a53c903d06
Yashaswi Gusain <gusainyashaswi@gmail.com>
2026-05-09 04:24:14 +0530

    Created New Section in Home Page

---
+++ client/src/pages/Home/AdminDashboardSection.jsx
@@ -0,0 +1,139 @@
+export default function AdminDashboardSection() {
+
+  return (
+
+    <section className="bg-black">
+
+      <div
+        className="
+          max-w-8xl
+    
+          mx-auto
+
+          px-6
+          md:px-10
+
+          py-32
+        "
+      >
+
+        <div
+          className="
+            grid
+            grid-cols-1
+            lg:grid-cols-2
+
+            items-center
+
+            gap-20
+          "
+        >
+
+          <div className="
+          m-auto
+
+          ">
+
+            <p
+              className="
+                uppercase
+
+                tracking-[3px]
+
+                text-xs
+
+                text-white
+              "
+            >
+              School Admin Dashboard
+            </p>
+
+            <h2
+              className="
+                mt-6    
+                mr-10
+
+                text-5xl
+                md:text-7xl
+
+                font-black
+
+                leading-[0.95]
+
+                tracking-[-4px]
+
+                text-white
+              "
+            >
+              Complete Control.
+              <br />
+              All In One Dashboard.
+            </h2>
+
+            <p
+              className="
+                mt-10
+
+                max-w-xl
+
+                text-lg
+
+                leading-9
+
+                text-white
+              "
+            >
+              Powerful tools to manage your
+              entire transport operation with
+              real-time tracking, reports,
+              monitoring and alerts.
+            </p>
+
+          </div>
+
+          <div
+            className="
+              relative
+              rounded-[40px]
+              mr-15
+
+              border
+              border-white/10
+
+              bg-white
+
+              p-6
+
+              shadow-2xl
+
+              min-h-[500px]
+            "
+          >
+
+            <div
+              className="
+                flex
+                items-center
+                justify-center
+
+                h-full
+
+                text-black/30
+
+                text-xl
+
+                font-medium
+              "
+            >
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+
+  );
+}
