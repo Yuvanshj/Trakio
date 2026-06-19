a9842adae95680173d0abe62f26afbadc1799cc3
Yashaswi Gusain <gusainyashaswi@gmail.com>
2026-06-19 12:37:34 +0530

    Update AboutHero.jsx

--- client/src/pages/About/AboutHero.jsx
+++ client/src/pages/About/AboutHero.jsx
@@ -14,21 +14,21 @@ export default function AboutHero() {
     {
       id: 1,
       name: "Yashaswi Gusain",
-      role: "UI/UX Lead",
+      role: "Design Lead",
       image: Yash,
       about: "Passionate about crafting immersive frontend experiences and modern UI systems.",
     },
     {
       id: 2,
       name: "Shresth Juyal",
-      role: "Backend Architect",
+      role: "Backend Learner",
       image: Alien,
       about: "Focused on scalable backend architecture, APIs, and performance optimization.",
     },
     {
       id: 3,
       name: "Yuvansh Juneja",
-      role: "Full Stack Developer",
+      role: "Full Stack Learner",
       image: Yuvi,
       about:"Designs elegant user experiences with strong attention to detail and storytelling.",
     }
